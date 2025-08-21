c40ca6020fb1cbb59fa12d75d3f65a25ca5b9a2e
windd <koredeodubanjo1@gmail.com>
2025-08-21 15:35:09 +0100

    First commit

---
+++ script/DeployRentPaymentSystem.s.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier:MIT
+
+import {Script} from "forge-std/Script.sol";
+import {RentPaymentSystem} from "../src/RentPaymentSystem.sol";
+
+pragma solidity ^0.8.19;
+
+contract DeployRentPaymentSystem is Script {}
