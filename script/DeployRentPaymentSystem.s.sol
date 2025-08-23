5d7072378c7007fe224982c99288f1984d26ccdf
windd <Koredeodubanjo1@gmail.com>
2025-08-23 17:29:20 +0100

    Update DeployRentPaymentSystem.s.sol

--- script/DeployRentPaymentSystem.s.sol
+++ script/DeployRentPaymentSystem.s.sol
@@ -1,8 +1,21 @@
-//SPDX-License-Identifier:MIT
+//SPDX-License-Identifier:MIT   
+pragma solidity ^0.8.19;    
 
-import {Script} from "forge-std/Script.sol";
-import {RentPaymentSystem} from "../src/RentPaymentSystem.sol";
+import {Script} from "forge-std/Script.sol";  
+import {RentPaymentSystem} from "../src/RentPaymentSystem.sol";  
+import {HelperConfig} from "./HelperConfig.s.sol";     
 
-pragma solidity ^0.8.19;
+contract DeployRentPaymentSystem is Script {   
 
-contract DeployRentPaymentSystem is Script {}
+    function run() external returns (RentPaymentSystem, HelperConfig) {                  
+        HelperConfig helperConfig = new HelperConfig();                   
+        address priceFeed = helperConfig.getConfigByChainId(block.chainid).priceFeed;                   
+        
+        vm.startBroadcast();                  
+        RentPaymentSystem rentPaymentSystem = new RentPaymentSystem(priceFeed);                  
+        vm.stopBroadcast();                  
+        
+        return (rentPaymentSystem, helperConfig);          
+    }
+
+}
